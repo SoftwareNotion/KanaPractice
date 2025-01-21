ed652da3581306fa15f53891712e01771a6cf588
Notion <53023383+SoftwareNotion@users.noreply.github.com>
2025-01-21 15:11:14 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          100: '#FAFAFA',
+          200: '#E4E5F1',
+          300: '#D2D3DB',
+          400: '#9394A5',
+          500: '#484B6A'
+        }
+      }
+    },
+  },
+  plugins: [],
+};
